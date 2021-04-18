9aaa073c733fd48ebc928332274476d03d43b89a
RedSeal9 <30299681+RedSeal9@users.noreply.github.com>
2021-04-18 10:36:11 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd $SERVER
 ./steamcmd.sh +runscript tf2_ds.txt
+mkdir $USER/.steam/sdk32
 ln -s $SERVER/linux32/steamclient.so $USER/.steam/sdk32/steamclient.so
 cd tf2
 chmod +x srcds_run srcds_linux 
