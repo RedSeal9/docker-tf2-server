46c91948b14ec8deb7fce63a688ed04c6aca51e2
RedSeal9 <red@redseal.red>
2021-04-17 23:07:11 -0400

    updated a ton of stuff

--- tf.sh
+++ tf.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cd $SERVER
-./update.sh
 cd tf2
-srcds_run -game tf -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/tf2_ds.txt $@
+if [ -f ".inst" ]; then
+    echo fount .inst, starting game server as normal
+    ./srcds_run -game tf -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/tf2_ds.txt $@
+else
+    echo did not find .inst, installing tf2 server and restarting script
+    cd ..
+    ./update.sh && touch tf2/.inst && $0
+fi
