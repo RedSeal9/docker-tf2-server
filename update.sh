d4d741e0da79e8c95a09c4333dd8b5672f05fe0c
RedSeal9 <red@redseal.red>
2021-05-01 08:42:41 -0400

    push to github to allow desktop to sync with server

--- update.sh
+++ update.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-<<<<<<< HEAD
 cd $SERVER
 ./steamcmd.sh +runscript $SERVER/tf2_ds.txt
-=======
-cd "$SERVER" || exit 1
-"$SCMD"/steamcmd.sh +runscript "$SERVER"/tf2_ds.txt
->>>>>>> 9a477424b974a7d64542677d6000681901c43483
